e3cc460bf3f281f043d256c51a7cd6d0494963da
Yuhao <syh19961001@gmail.com>
2016-03-02 12:25:26 -0800

    change

--- server.R
+++ server.R
@@ -1,11 +1,36 @@
 library(shiny)
+library(dplyr)
+library(plotly)
+source('assignment.r')
 
 # Code to be executed by the shiny server
 shinyServer(function(input, output) {
   # input: information passed from the UI into the R session
-  
+ # lengthx <- paste0(input$type, '.', "Length")
+#  widthx <- paste0(input$type, '.', "Width")
+ # value <- filter(data_summary, species = input$Species) %>%
+  #          select(lengthx, widthx)
   # output: objects passed from R session back to the server
   
-  # Leaving this blank for now
-  
+  #Create an adjustable bar chart
+  output$graph <- renderPlotly({
+    
+    #Change the data when users click different species button
+    data_species <- switch(input$Species,
+                          "setosa" = filter(data_summary, Species == "setosa"),
+                          "versicolor" = filter(data_summary, Species == "versicolor"),
+                          "virginica" = filter(data_summary, Species == "virginica"))
+    
+    #Change the data when users choose different type of data they want to see(sepal or petal)
+    data_type <- switch(input$type,
+                        "type1" = select(data_species, paste0(input$type, '.', "Length"), 
+                                         paste0(input$type, '.', "Width")),
+                        "type2" = select(data_species, paste0(input$type, '.', "Length"), 
+                                         paste0(input$type, '.', "Width")))
+    
+    #Create the bar chart that can adjusts showing data based on users' choice
+    p <- plot_ly(data_summary, x = c(paste("Average", input$type, "Length"), 
+                                     paste("Average", input$type, "Width")), 
+                 y = data_type[1,], type = "bar", name = "Average Length and Width")
+  })
 })
--- ui.R
+++ ui.R
@@ -1,11 +1,29 @@
 library(shiny)
+library(plotly)
+library(dplyr)
+source('assignment.r')
 
 # Code to be executed by the shiny server
-shinyServer(function(input, output) {
-  # input: information passed from the UI into the R session
+shinyUI(fluidPage(
+  titlePanel("iris dataset"),
+  sidebarLayout(
+    sidebarPanel(
+      #Choose which type of data to display, sepal or petal
+      selectInput("type", label = h3("display choice")),
+        choices = list("Sepal" = "Sepal", 
+                       "Petal" = "Petal"),
+        selected = "Sepal"),
+      
+      hr(),
+      
+      #Choose which species to display
+      radioButtons("Species", label = h3("Specie type")),
+        choices = list("setosa" = "setosa", "versicolor" = "versicolor", "virginica" = "virginica"),
+        selected = "setosa")
+    ),
   
-  # output: objects passed from R session back to the server
-  
-  # Leaving this blank for now
-  
-})
+    #Display the graph
+    mainPanel(
+      plotlyOutput("graph")
+    )
+  )

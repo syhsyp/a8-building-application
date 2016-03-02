d7b92d175cfc8d032061dcaab34880c3c4e49bc5
Yuhao <syh19961001@gmail.com>
2016-03-02 15:07:09 -0800

    1

--- assignment.r
+++ assignment.r
@@ -2,12 +2,14 @@ library(dplyr)
 library(plotly)
 
 data('iris')
+#create a summary table that shows average measument for each kind of data
 data_summary <- group_by(iris, Species) %>%
             summarise(Sepal_Length = mean(Sepal.Length, na.rm = TRUE), 
                       Sepal_Width = mean(Sepal.Width, na.rm = TRUE), 
                       Petal_Length = mean(Petal.Length, na.rm = TRUE), 
                       Petal_Width = mean(Petal.Width, na.rm = TRUE))
 
+#a function to create a bar chart with dataframe, data type and species
 build_plot <-function(df, type, species) {
   df <- filter(df, Species == species) %>%
                 select_(paste0(type, "_Length"), paste0(type, "_Width"))
